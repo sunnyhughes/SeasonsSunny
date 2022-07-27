0e1acfea6284a9d95083a84b57a4f0717f2350fb
Sunshine Hughes <sunshinehughes2011@gmail.com>
2022-07-27 13:31:06 -0400

    tweaking the page

---
+++ javascript.js/site.js
@@ -0,0 +1,71 @@
+/* This is the menu */
+
+function toggleMenu() {
+    document.getElementById("topNav").classList.toggle ("open")
+    ("open")
+}
+
+let x = document.getElementById('hamburgerBtn');
+    x.onclick = toggleMenu;
+
+ /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
+function myFunction() {
+    var x = document.getElementById("topNav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+/* this is javascript for the photo wall */
+
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";  // IE10
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
